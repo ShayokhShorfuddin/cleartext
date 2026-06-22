e5d7d9622c2bbd16d9e67cbc5337b7a743fe1d0b
Shayokh <shayokhshuvro14@gmail.com>
2026-06-22 23:40:45 +0600

    feat: intial success in parsing markdown content

--- app/validate/page.tsx
+++ app/validate/page.tsx
@@ -1,16 +1,41 @@
+"use client";
+
+import { useRef } from "react";
+import remarkParse from "remark-parse";
+import { unified } from "unified";
+
 export default function Page() {
-  // TODO: Implement validator here
+  // TODO: Implement validator on this page
+
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  function checkIfItsParsingOrNot() {
+    console.log("Fired");
+
+    if (!textareaRef.current) {
+      return;
+    }
+
+    const content = textareaRef.current.value;
+
+    const tree = unified().use(remarkParse).parse(content);
+
+    console.log(tree);
+  }
+
   return (
     <main className="flex flex-col items-center">
       <textarea
         name="content"
         id="paste-content"
+        ref={textareaRef}
         placeholder={"Paste your llms.txt content"}
         className="border border-neutral-300 w-1/2 h-50 mt-10"
       />
 
       <button
         type="button"
+        onClick={checkIfItsParsingOrNot}
         className="p-2 border border-neutral-200 mt-2 cursor-pointer"
       >
         Validate
@@ -18,3 +43,35 @@ export default function Page() {
     </main>
   );
 }
+
+// Great!
+// For this:
+
+// # Title
+
+// > Optional description goes here
+
+// We are getting something like this:
+
+// {type: 'root', children: Array(2), position: {…}}
+// children
+// :
+// Array(2)
+// 0
+// :
+// {type: 'heading', depth: 1, children: Array(1), position: {…}}
+// 1
+// :
+// {type: 'blockquote', children: Array(1), position: {…}}
+// length
+// :
+// 2
+// [[Prototype]]
+// :
+// Array(0)
+// position
+// :
+// {start: {…}, end: {…}}
+// type
+// :
+// "root"
